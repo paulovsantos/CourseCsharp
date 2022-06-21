a493a8fb38318987b90c164ed031e46a08fc8983
paulovsantos <paulo.vitors@hotmail.com>
2022-06-21 20:48:45 -0300

    Adicionando projeto inicial

---
+++ CourseCsharp/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace CourseCsharp.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        junior = 0,
+        Midlevel = 1,
+        Senior = 2
+    }
+}
---
+++ CourseCsharp/Entities/Worker.cs
@@ -0,0 +1,26 @@
+﻿using CourseCsharp.Entities.Enums;
+using System.Collections.Generic;
+
+namespace CourseCsharp.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+    }
+}
